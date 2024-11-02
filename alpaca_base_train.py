8543f8665d572a578c60a6fd7ea7c2f799c40168
wonjunlee <ayjmlo6040@gmail.com>
2024-11-02 18:00:36 +0900

    2

--- alpaca_base_train.py
+++ alpaca_base_train.py
@@ -98,7 +98,7 @@ def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedToken
         tokenizer(
             text,
             return_tensors="pt",
-            padding="longest",
+            padding="max_length",
             max_length=tokenizer.model_max_length,
             truncation=True,
         )
@@ -193,9 +193,9 @@ def train():
 
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_args.model_name_or_path,
-        # torch_dtype=torch.bfloat16,
+        torch_dtype=torch.bfloat16,
         # attn_implementation="flash_attention_2",
-        # use_cache=False,
+        use_cache=False,
         cache_dir=training_args.cache_dir,
     )
 
