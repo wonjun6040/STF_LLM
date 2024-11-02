546ce5cb3cddd23260a693e3aa78834f3a771493
wonjunlee <ayjmlo6040@gmail.com>
2024-11-02 17:21:05 +0900

    first commit

---
+++ alpaca_base.sh
@@ -0,0 +1,22 @@
+torchrun --nproc_per_node=2 alpaca_base_train.py \
+    --model_name_or_path "Bllossom/llama-3.2-Korean-Bllossom-3B" \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir aplaca_base \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True \
+    --report_to "tensorboard"
